a5caca699241ad878853f63faf17e6975b864a44
safeincraziworld <hi.longnu@gmail.com>
2023-03-11 16:40:03 +0800

    update auto_increment

--- SmartBinWebsite.Python/Server/BinLogDAO.py
+++ SmartBinWebsite.Python/Server/BinLogDAO.py
@@ -1,5 +1,22 @@
+import mysql.connector
+  
+mydb = mysql.connector.connect(
+    host = 'fuqianshan.asuscomm.com',
+    user = 'fuqianshan',
+    password = 'KRPCGroup',
+    database = 'smartbin'
+)
+  
+cursor = mydb.cursor()
+
 class BinLogDAO:
 
     #register the bin into the database and return BID
-    def BinRigister(latitude,longtitude):
-        pass
+    def BinRigister(latitude,longitude):
+        statement = 'INSERT INTO bin (latitude, longitude) VALUES (%s, %s)'
+        value = (latitude, longitude)
+
+        cursor.execute(statement, value)
+        mydb.commit()
+
+        return cursor.lastrowid
---
+++ SmartBinWebsite.Python/Server/checkid.py
@@ -0,0 +1,4 @@
+from BinLogDAO import *
+
+id = BinLogDAO.BinRigister(15, 20)
+print(id)
