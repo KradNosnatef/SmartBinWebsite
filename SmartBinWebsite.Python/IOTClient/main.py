bb2638211a32d9b5214ff77000a0805c55e80064
KradNosnatef <fuqianshan@foxmail.com>
2023-03-11 16:41:01 +0800

    test

--- SmartBinWebsite.Python/IOTClient/main.py
+++ SmartBinWebsite.Python/IOTClient/main.py
@@ -1,15 +1,23 @@
 import requests
 import serial
 import json
+import re
 
 serial = serial.Serial()
 serial.baudrate = 115200
 serial.port = '/dev/cu.usbmodem11302'
 serial.open()
 while True:
-    value = serial.readline()
-    print(value.decode())
+    try:
+        value = serial.readline()
+        data1=re.search("[a-z]+:([0-9]|[a-z]|[A-Z])*",value.decode()).group()
 
-    data={'data':value.decode()}
-    jsonData=json.dumps(data)
-    requests.post("http://fuqianshan.asuscomm.com:22000/Xapi",data=data)
+        data2=data1.split(":",1)
+        data3={}
+
+        data3[data2[0]]=data2[1]
+        print(data3)
+    except:
+        pass
+    #jsonData=json.dumps(data)
+    #requests.post("http://fuqianshan.asuscomm.com:22000/Xapi",data=data)
