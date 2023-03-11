7ce5374e1d89657a541f978bb9480363c53cc10f
KradNosnatef <fuqianshan@foxmail.com>
2023-03-11 16:55:35 +0800

    test

--- SmartBinWebsite.Python/IOTClient/main.py
+++ SmartBinWebsite.Python/IOTClient/main.py
@@ -10,13 +10,15 @@ serial.open()
 while True:
     try:
         value = serial.readline()
-        data1=re.search("[a-z]+:([0-9]|[a-z]|[A-Z])*",value.decode()).group()
+        data1=re.search("[a-z]+:([0-9]|[a-z]|[A-Z]|.)*",value.decode()).group()
 
         data2=data1.split(":",1)
         data3={}
 
-        data3[data2[0]]=data2[1]
-        print(data3)
+        data3[data2[0]]=float(data2[1])
+
+        jsonData=json.dumps(data3)
+        print("jsonData is : "+jsonData)
     except:
         pass
     #jsonData=json.dumps(data)
