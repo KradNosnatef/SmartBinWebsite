551d9fcd86dadd8d740e31a21c936ab655e8c60e
KradNosnatef <fuqianshan@foxmail.com>
2023-03-14 18:37:36 +0800

    debug

--- SmartBinWebsite.Python/IOTClient/main.py
+++ SmartBinWebsite.Python/IOTClient/main.py
@@ -11,18 +11,7 @@ while True:
     try:
         value = serial.readline()
         print(value.decode())
-        obj=json.loads(value.decode().rstrip("\r\n"))
-        print(obj["fuck"])
-        """data1=re.search("[a-z]+:([0-9]|[a-z]|[A-Z]|.)*",value.decode()).group()
-
-        data2=data1.split(":",1)
-        data3={}
-
-        data3[data2[0]]=float(data2[1])
-
-        jsonData=json.dumps(data3)
-        print("jsonData is : "+jsonData)"""
+        jsonString=value.decode().rstrip("\r\n")
     except:
         pass
-    #jsonData=json.dumps(data)
     #requests.post("http://fuqianshan.asuscomm.com:22000/Xapi",data=data)
