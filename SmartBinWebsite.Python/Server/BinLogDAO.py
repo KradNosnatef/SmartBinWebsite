38824de83d87afc72a2e33a6c02cfc321afe7c7e
KradNosnatef <fuqianshan@foxmail.com>
2023-03-16 21:10:12 +0800

    debug

--- SmartBinWebsite.Python/Server/BinLogDAO.py
+++ SmartBinWebsite.Python/Server/BinLogDAO.py
@@ -7,23 +7,25 @@ mydb = mysql.connector.connect(
     password = 'KRPCGroup',
     database = 'smartbin'
 )
-  
-cursor = mydb.cursor()
 
 class BinLogDAO:
 
     #register the bin into the database and return BID
     def BinRigister(latitude,longitude):
+        cursor=mydb.cursor()
         statement = 'INSERT INTO bin (latitude, longitude) VALUES (%s, %s)'
         value = (latitude, longitude)
 
         cursor.execute(statement, value)
         mydb.commit()
+        result=cursor.lastrowid
+        cursor.close()
 
         return cursor.lastrowid
     
 
     def insertRawInfo(BID, weight, distance, pitch, roll):
+        cursor=mydb.cursor()
         now = datetime.datetime.now()
         timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
         statement = 'INSERT INTO rawInfo (BID, weight, distance, pitch, roll, timestamp) VALUES (%s, %s, %s, %s, %s, %s)'
@@ -32,9 +34,11 @@ class BinLogDAO:
         cursor.execute(statement, value)
         mydb.commit()
 
+        cursor.close()
         return 0
 
     def insertAlert(BID, text):
+        cursor=mydb.cursor()
         now = datetime.datetime.now()
         timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
         statement = 'INSERT INTO alert (BID, text, timestamp) VALUES (%s, %s, %s)'
@@ -43,9 +47,12 @@ class BinLogDAO:
         cursor.execute(statement, value)
         mydb.commit()
 
+        cursor.close()
         return 0
     
     def getBinBriefing():
+        
+        cursor=mydb.cursor()
         sql="SELECT bin.BID,bin.Latitude,bin.Longitude,TB.nearestAlert from bin left join (select BID, substring_index(group_concat(text order by Timestamp desc),',',1) as nearestAlert from alert group by BID) as TB on bin.BID=TB.BID;"
         cursor.execute(sql)
         result=cursor.fetchall()
@@ -53,15 +60,25 @@ class BinLogDAO:
 
         result2=[{"BID":row[0],"latitude":row[1],"longitude":row[2],"text":row[3] if row[3]!=None else "No alert yet"} for row in result]
 
-
+        cursor.close()
         return result2
     
     def getBinAlertListByBID(BID):
-        sql="SELECT * from alert order by Timestamp desc where BID=%s;"
+        cursor=mydb.cursor()
+        sql="SELECT * from alert where BID=%s order by Timestamp desc;"
         val=(BID,)
-        cursor.execute(sql)
+        cursor.execute(sql,val)
 
         result=cursor.fetchall()
+        cursor.close()
         return result
     
-BinLogDAO.getBinAlertListByBID(15)
+    def getBinRawInfoByBID(BID):
+        cursor=mydb.cursor()
+        sql="SELECT * from rawInfo where BID=%s order by Timestamp desc;"
+        val=(BID,)
+        cursor.execute(sql,)
+
+        result=cursor.fetchall()
+        cursor.close()
+        return result
