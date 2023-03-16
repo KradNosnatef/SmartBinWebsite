c89b9bbd48bdc9d53e3cd2d448b470b3194eed6d
KradNosnatef <fuqianshan@foxmail.com>
2023-03-16 21:33:35 +0800

    debug

--- Vue/pages/centralMonitoring/BinInfoDialog.vue
+++ Vue/pages/centralMonitoring/BinInfoDialog.vue
@@ -6,23 +6,34 @@
 		<p>Longitude: {{dialogSettings.lng}}</p>
 		<p>Latitude: {{dialogSettings.lat}}</p>
 		<p>Most Recent Alert: {{dialogSettings.recentAlert}}</p>
+		<button @click="onClick">test</button>
 	</div>
 </template>
 
 <script>
 	export default {
-		props:{
-			dialogSettings:Object
+		props: {
+			dialogSettings: Object
 		},
 		data() {
 			return {
-				
+
 			}
 		},
-		onShow(){
-		},
+		onShow() {},
 		methods: {
-			
+			onClick() {
+				uni.request({
+					url: "/Wapi/getAlertsByBID",
+					method: 'POST',
+					data: {
+						BID: 15
+					},
+					success: (response) => {
+						console.log(response)
+					}
+				})
+			}
 		}
 	}
 </script>
